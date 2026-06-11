6b7149ea83373a94c2dda590f21b38943a817cf2
Tom984-vn <dangthantheson2006@gmail.com>
2026-06-12 01:40:24 +0700

    fix merge

--- java_core/GUI/src/main/java/com/disaster/analysis/TaskAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package com.disaster.analysis;
-
-import com.disaster.ai_client.AiClient;
-import com.disaster.datacollection.model.SocialMediaPost;
-import java.util.List;
-
-public interface TaskAnalyzer {
-    AnalysisResult analyze(List<SocialMediaPost> posts, AiClient aiClient);
-}
